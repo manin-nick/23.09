08629db5807c5bc5bffac245568881293da4193e
manin-nick <manin.nick@gmail.com>
2024-09-30 22:06:12 +0400

    fix LanguageProvider

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,11 @@
 import { useContext, useEffect, useState } from 'react';
-import { PostList } from './components/PostList'; 
-import { LanguageSwitcher } from './components/LanguageSwitcher';
+import { PostList } from './components/PostList';
+import LanguageSwitcher from './components/LanguageSwitcher';
 import LanguageContext from './components/LanguageContext';
-
-
-
+import { LanguageProvider } from './components/LanguageContext';
 
 function App() {
-  
   const [data, setData] = useState([]);
-  const { language } = useContext(LanguageContext);
 
   const fetchData = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -27,31 +23,20 @@ function App() {
 
   return (
     <>
-    <div>
-      <button className="get-data-btn" onClick={handleButtonClick}>
-        Click
-      </button>
-      {!!data.length && <PostList data={data} />}
-    </div>
+      <div>
+        <button className="get-data-btn" onClick={handleButtonClick}>
+          Click
+        </button>
+        {!!data.length && <PostList data={data} />}
+      </div>
 
-    <div>
-<LanguageProvider>
-  <LanguageSwitcher />
-<LanguageProvider/>
-</div>
-
-  </>
+      <div>
+        <LanguageProvider>
+          <LanguageSwitcher/>
+        </LanguageProvider>
+      </div>
+    </>
   );
-
 }
-
-
-  
-
-
-
-
-
-
 
 export default App;
